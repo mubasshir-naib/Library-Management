218de2b1f0fb4840dcb0a154278495e5b8f8880e
mubasshir-naib <mubasshir.naib@brainstation-23.com>
2025-08-26 15:07:53 +0600

    added some basic packages

--- LibraryManagement.Application/DependencyInjection.cs
+++ LibraryManagement.Application/DependencyInjection.cs
@@ -12,6 +12,7 @@ namespace LibraryManagement.Application
     {
         public static IServiceCollection AddApplicationDI(this IServiceCollection services)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
             return services;
         }
     }
--- LibraryManagement.Infrastructure/obj/Debug/net8.0/LibraryManagement.Infrastructure.AssemblyInfo.cs
+++ LibraryManagement.Infrastructure/obj/Debug/net8.0/LibraryManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+598bd66fac41eb092b6bf0368992dc76a4f4ad4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
